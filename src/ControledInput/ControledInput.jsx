4a5d65363c3cd58a326489cb28df0017f7d5dca4
Krishnam-Upadhyay <upadhyaykrishnam21@gmail.com>
2023-07-17 13:42:37 +0530

    1 file modified

--- src/ControledInput/ControledInput.jsx
+++ src/ControledInput/ControledInput.jsx
@@ -1,32 +1,20 @@
 import { useState } from 'react';
+
 const ControledInput = () => {
   const [name, setName] = useState('');
-  const [user, setUser] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleNameChange = (e) => {
-    const value = e.target.value;
-    setName(value);
-  };
-  const handleEmailChange = (e) => {
-    const value = e.target.value;
-    setEmail(value);
-  };
   const handleSubmitChange = (e) => {
     e.preventDefault();
     if (!name) {
       return;
     }
-    const fakeUser = Date.now();
-    const newUser = { id: fakeUser, name };
-    const updatedUser = [...user, ...newUser];
-    setUser(updatedUser);
-
-    setShowEmail(email);
   };
-
   return (
     <form className="form" onSubmit={handleSubmitChange}>
-      <h4>Controled Input</h4>
+      <h4>Multiple Inputs</h4>
+
       <div className="form-row">
         <label htmlFor="Name" className="form-label">
           Name
@@ -39,16 +27,41 @@ const ControledInput = () => {
           onChange={handleNameChange}
           placeholder="Your Name"
         />
-        <br />
-
+      </div>
+      <br />
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          Email:
+        </label>
+        <input
+          type="email"
+          className="form-input"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+          placeholder="abc@gmail.com"
+        />
+      </div>
+      <div className="form-row">
+        <label htmlFor="Password" className="form-label">
+          Email:
+        </label>
+        <input
+          type="password"
+          className="form-input"
+          id="Password"
+          value={password}
+          onChange={handleEmailChange}
+          placeholder="abc@gmail.com"
+        />
+      </div>
+      <br />
+      <div className="form-row">
         <button type="submit" className="form-button">
           Submit
         </button>
-        <br />
-        <h4>{showName}</h4>
-
-        <h4>{showemail}</h4>
       </div>
+      <br />
     </form>
   );
 };
