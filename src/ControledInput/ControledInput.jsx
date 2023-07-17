d7731f9222a46921a1c3c2db80ae4b0c164e9cec
Krishnam-Upadhyay <upadhyaykrishnam21@gmail.com>
2023-07-17 13:53:56 +0530

    1 file modified, 1 added

--- src/ControledInput/ControledInput.jsx
+++ src/ControledInput/ControledInput.jsx
@@ -1,20 +1,28 @@
+import './app.css'
 import { useState } from 'react';
-
 const ControledInput = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [showemail, setShowEmail] = useState('');
+  const [showName, setShowName] = useState('');
 
+  const handleNameChange = (e) => {
+    const value = e.target.value;
+    setName(value);
+  };
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+  };
   const handleSubmitChange = (e) => {
     e.preventDefault();
-    if (!name) {
-      return;
-    }
+    setShowName(name);
+    setShowEmail(email);
   };
+
   return (
     <form className="form" onSubmit={handleSubmitChange}>
-      <h4>Multiple Inputs</h4>
-
+      <h4>Controled Input</h4>
       <div className="form-row">
         <label htmlFor="Name" className="form-label">
           Name
@@ -27,9 +35,8 @@ const ControledInput = () => {
           onChange={handleNameChange}
           placeholder="Your Name"
         />
-      </div>
-      <br />
-      <div className="form-row">
+        <br />
+
         <label htmlFor="email" className="form-label">
           Email:
         </label>
@@ -41,27 +48,15 @@ const ControledInput = () => {
           onChange={handleEmailChange}
           placeholder="abc@gmail.com"
         />
-      </div>
-      <div className="form-row">
-        <label htmlFor="Password" className="form-label">
-          Email:
-        </label>
-        <input
-          type="password"
-          className="form-input"
-          id="Password"
-          value={password}
-          onChange={handleEmailChange}
-          placeholder="abc@gmail.com"
-        />
-      </div>
-      <br />
-      <div className="form-row">
+        <br />
         <button type="submit" className="form-button">
           Submit
         </button>
+        <br />
+        <h4>{showName}</h4>
+
+        <h4>{showemail}</h4>
       </div>
-      <br />
     </form>
   );
 };
